b3260b69628a8380ef3b08c6964e4ca9079e8ebe
Khanh Vu <vungockhanh8anh@gmail.com>
2026-01-22 11:10:11 +0700

    Update code luật di chuyển quân Tượng - Vũ Ngọc Khánh

--- src/Bishop.cpp
+++ src/Bishop.cpp
@@ -2,25 +2,19 @@
 
 bool Bishop::Is_Valid_Move(int destRow, int destCol, const Board &grid)
 {
-    // hàm kiểm tra có vật cản hay không ???
-    auto Is_Path_Blocked = [&]() -> bool
+    if (abs(row - destRow) == abs(col - destCol)) // điểm đến có đúng luật không ???
     {
+        // Kiểm tra xem có vật cản đường không ???
         int stepRow = (destRow < row) ? -1 : 1; // di chuyển lên trên hay xuống dưới
         int stepCOl = (destCol > col) ? 1 : -1; // di chuyển sang trái hay phải
         int currR = row + stepRow, currC = col + stepCOl;
         while (currR != destRow && currC != destCol)
         {
             if (grid.Get_Piece_At(currR, currC) != nullptr)
-                return true;
+                return false;
             currR += stepRow;
             currC += stepCOl;
         }
-        return false;
-    };
-    if (abs(row - destRow) == abs(col - destCol)) // điểm đến có đúng luật không ???
-    {
-        if (Is_Path_Blocked()) // có vật cản
-            return false;
         return true;
     }
     return false;
