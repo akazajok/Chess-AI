d92420081fe0eecbf553c50fbf704ff52678ca85
KanzuNe <hieu04092007@gmail.com>
2026-01-30 21:10:05 +0700

    return 0 :v

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,5 @@ using namespace std;
 
 int main()
 {
-    Board chessGame;
-    
-    cout << "Simple castling test:\n";
-    string test = "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQkq - 0 1";
-    chessGame.Set_Up_Board(test);
-    
-    cout << "Before castling:\n";
-    chessGame.Display();
-    
-    cout << "Attempting White Kingside Castling...\n";
-    chessGame.Execute_Move(7, 4, 7, 6);
-    
-    cout << "After castling:\n";
-    chessGame.Display();
-    
     return 0;
 }
