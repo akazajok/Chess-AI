26133cff9f407760bcaddece90387ca4f021e58e
Khanh Vu <vungockhanh8anh@gmail.com>
2026-02-27 13:28:34 +0700

    Merge code NguyenHieu giai đoạn 4

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     // Chuỗi FEN khởi tạo bàn cờ mặc định
-    std::string defaultFEN = "7k/8/8/8/1P6/8/2P5/1K6 w - - 0 1";
+    std::string defaultFEN = "1k6/8/8/8/8/8/4P3/1K6 w - - 0 1";
 
     gameManager manager;
     manager.Init_Game(defaultFEN); // Nạp dữ liệu vào bàn cờ bên trong manager
